15326361642c1830cfeff6a7810af9ed625a052b
Julian Kingman <julian@designman.io>
2017-07-29 10:37:59 -0400

    starting from scratch

---
+++ wp-support-plus-api.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin Name:       WP Support Plus API
+ * Description:       REST API Access to WP Support Plus
+ * Version:           0.0.1
+ * Author:            Designman
+ * Author URI:        http://designman.io/
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+ 
